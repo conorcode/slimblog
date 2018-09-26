1a0558b9a641696cc1232cb736d15ffa4bbef279
conorcode <conor345gr@gmail.com>
2018-09-26 15:11:34 +0300

    Slimblog First Commit

---
+++ app/api/category.php
@@ -0,0 +1,118 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$app = new \Slim\App;
+
+// Get All Categories
+$app->get('/api/categories', function (Request $request, Response $response) {
+    $sql = "SELECT * FROM categories";
+
+    try {
+        // Get Database Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->query($sql);
+        $categories = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($categories);
+    } catch (PDOEception $e) {
+        echo '{"error": {"text":'.$e->getMessage().'}}';
+    }
+});
+
+// Get Single Category
+$app->get('/api/category/{id}', function (Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+
+    $sql = "SELECT * FROM categories WHERE id = $id";
+
+    try {
+        // Get Database Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->query($sql);
+        $category = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($category);
+    } catch (PDOEception $e) {
+        echo '{"error": {"text":'.$e->getMessage().'}}';
+    }
+});
+
+// Add Category
+$app->post('/api/category/add', function (Request $request, Response $reponse) {
+    $name = $request->getParam('name');
+
+    $sql = "INSERT INTO categories (name) VALUES (:name)";
+
+    try {
+        // Get Database Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindParam(':name', $name);
+
+        $stmt->execute();
+
+        echo '{"notice": {"text":"Category Added"}}';
+    } catch (PDOEception $e) {
+        echo '{"error": {"text":'.$e->getMessage().'}}';
+    }
+});
+
+// Update Category
+$app->put('/api/category/update/{id}', function (Request $request, Response $reponse) {
+    $id = $request->getAttribute('id');
+
+    $name = $request->getParam('name');
+
+    $sql = "UPDATE categories SET
+							name 		= :name
+						WHERE id = $id";
+
+    try {
+        // Get Database Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindParam(':name', $name);
+
+        $stmt->execute();
+
+        echo '{"notice": {"text":"Category '.$id.' Updated"}}';
+    } catch (PDOEception $e) {
+        echo '{"error": {"text":'.$e->getMessage().'}}';
+    }
+});
+
+// Delete Category
+$app->delete('/api/category/delete/{id}', function (Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+
+    $sql = "DELETE FROM categories WHERE id = $id";
+
+    try {
+        // Get Database Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        $db = null;
+        echo '{"notice": {"text":"Category '.$id.' Deleted"}}';
+    } catch (PDOException $e) {
+        echo '{"error": {"text":'.$e->getMessage().'}}';
+    }
+});
---
+++ app/db.php
@@ -0,0 +1,18 @@
+<?php
+class db
+{
+    // Properties
+    private $dbhost = "localhost";
+    private $dbuser = "root";
+    private $dbpass = "";
+    private $dbname = "slimblog";
+
+    // Connect to DB
+    public function connect()
+    {
+        $mysql_conn_string = "mysql:host=$this->dbhost;dbname=$this->dbname";
+        $dbConnection = new PDO($mysql_conn_string, $this->dbuser, $this->dbpass);
+        $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $dbConnection;
+    }
+}
